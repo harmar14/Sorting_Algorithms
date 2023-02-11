bac229e101940b1c7471d9e3f5931d2e8c3583ca
harmar14 <juliabelokon00@mail.ru>
2023-02-12 00:27:10 +0300

    bubble sort optimization

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -4,10 +4,14 @@ public class BubbleSort {
     public static ArrayList execute(ArrayList <Integer> dataSet, String mode) {
 
         ArrayList <Integer> array = new ArrayList(dataSet); // we should not change an original array
-        boolean completed = false;
+        // boolean completed = false;
+        int cnt = 0;
+        int k = 1;
+        int j = array.size();
 
         switch (mode) {
             case ("asc"):
+                /*
                 while (!completed) {
                     completed = true;
                     for (int i = 1; i < array.size(); i++) {
@@ -20,8 +24,24 @@ public class BubbleSort {
                         }
                     }
                 }
+                */
+                while (k > 0) {
+                    k = 0;
+                    for (int i = 1; i < j; i++) {
+                        int previous = array.get(i - 1);
+                        int current = array.get(i);
+                        if (previous > current) {
+                            array.set(i - 1, current);
+                            array.set(i, previous);
+                            k = i;
+                        }
+                        cnt++;
+                    }
+                    j = k;
+                }
                 break;
             case ("desc"):
+                /*
                 while (!completed) {
                     completed = true;
                     for (int i = 1; i < array.size(); i++) {
@@ -33,6 +53,21 @@ public class BubbleSort {
                             completed = false;
                         }
                     }
+                }
+                */
+                while (k > 0) {
+                    k = 0;
+                    for (int i = 1; i < j; i++) {
+                        int previous = array.get(i - 1);
+                        int current = array.get(i);
+                        if (previous < current) {
+                            array.set(i - 1, current);
+                            array.set(i, previous);
+                            k = i;
+                        }
+                        cnt++;
+                    }
+                    j = k;
                 }
                 break;
             default:
